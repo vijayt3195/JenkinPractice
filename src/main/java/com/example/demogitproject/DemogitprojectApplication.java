4110b7384cdf0aa10c841f11ec6c8b67c3450a00
fasoftwares <vijay.t@fasotwares.com>
2020-03-23 14:25:27 +0530

    new demo repo

---
+++ src/main/java/com/example/demogitproject/DemogitprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demogitproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemogitprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemogitprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demogitproject/DemogitprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demogitproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemogitprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
